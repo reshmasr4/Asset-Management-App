ae5657b0395bf91251159a8173b35863a3339056
codemuffin <74981247+reshmasr4@users.noreply.github.com>
2024-12-29 16:14:21 +0100

    test update

--- client/src/test/playwright.config.ts
+++ client/src/test/playwright.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   timeout: 30000, 
   retries: 1, 
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:3000/',
+    timeout: 120000,
+  },
   use: {
+    baseURL: 'http://localhost:3000/',
     headless: true,
     screenshot: 'only-on-failure', // Take a screenshot only when a test fails
     trace: 'on-first-retry', 
